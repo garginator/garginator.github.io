4df1b845a474d6f8158944cb5f34608dd8d966c3
garginator <garginator@gmail.com>
2017-04-14 14:48:42 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 function light(){
-	var apikey = "EWRCRYWKHZG5R19S";
+	var apikey = "VBANF87J758CB1MA";
 	var status = document.getElementById('light_status').value;
-	window.open("https://api.thingspeak.com/update.json?api_key=" + apikey + "&field1=" + status,'_blank');
+	window.open("https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status,'_blank');
 	//$.getJson("https://thingspeak.com/channels/249150.json")
 	window.location.reload();
 	return false;
