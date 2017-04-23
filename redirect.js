e43392e762b1b63b14031fd5605b16d3990004a4
garginator <garginator@gmail.com>
2017-04-23 23:26:26 +0530

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -1,8 +1,64 @@
-function light(){
+var res;
+var f3;
+function light(v){
 	var apikey = "VBANF87J758CB1MA";
-	var status = document.getElementById('light_status').value;
-	window.open("https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status,'_blank');
+	var status = v;
+	var loc = "https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status;
+	//window.open("https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status,'_blank');
 	//$.getJson("https://thingspeak.com/channels/249150.json")
-	window.location.reload();
+	//window.location.reload();
+	
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       // Typical action to be performed when the document is ready:
+	       res = xhttp.responseText;
+	       var a = JSON.parse(res);
+	       f3 = a.field3;
+	       if(f3 == 1){
+	       		$('body').addClass("night");
+	       }
+	       else if(f3 == 0)
+	       {
+				$('body').removeClass("night");	
+	       }
+
+	       //document.getElementById("demo").innerHTML = res;
+	    }
+	};
+	xhttp.open("GET", loc, true);
+	xhttp.send();
 	return false;
-	}
+}
+
+function light1(v){
+	var apikey = "VBANF87J758CB1MA";
+	var status = v;
+	//var loc = "https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status;
+	var loc = "https://thingspeak.com/channels/256692/fields/3/last.json";
+	//window.open("https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status,'_blank');
+	//$.getJson("https://thingspeak.com/channels/249150.json")
+	//window.location.reload();
+	
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       // Typical action to be performed when the document is ready:
+	       res = xhttp.responseText;
+	       var a = JSON.parse(res);
+	       f3 = a.field3;
+	       if(f3 == 1){
+	       		$('body').addClass("night");
+	       		console.log("yes");
+	       }
+	       else if(f3 == 0)
+	       {
+	       		$('body').removeClass('night');
+	       }
+	       //document.getElementById("demo").innerHTML = res;
+	    }
+	};
+	xhttp.open("GET", loc, true);
+	xhttp.send();
+	return false;
+}
---
+++ redirect_1.js
@@ -0,0 +1,49 @@
+var res;
+var f3 = 2;
+function light(loc){
+	//var apikey = "VBANF87J758CB1MA";
+	//var status = document.getElementById('light_status').value;
+	//var loc = "https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status;
+	//window.open("https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status,'_blank');
+	//$.getJson("https://thingspeak.com/channels/249150.json")
+	//window.location.reload();
+	
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       // Typical action to be performed when the document is ready:
+	       res = xhttp.responseText;
+	       var a = JSON.parse(res);
+	       f3 = a.field3;
+	       if(f3 == 1){
+	       		document.getElementById("indicator").style.background = "green";
+	       }
+	       else if(f3 == 0)
+	       {
+	       		document.getElementById("indicator").style.background = "red";
+	       }
+	       else
+	       {
+
+	      		document.getElementById("indicator").style.background = "purple";
+
+	       }
+	       //document.getElementById("demo").innerHTML = res;
+	    }
+	};
+	xhttp.open("GET", loc, true);
+	xhttp.send();
+	return false;
+}
+
+function callingfn(){
+	var status = document.getElementById('light_status').value;
+	var apikey = "VBANF87J758CB1MA";
+	var loc = "https://api.thingspeak.com/update.json?api_key=" + apikey + "&field3=" + status;
+	light(loc);
+}
+
+function first(){
+	var loc = "https://thingspeak.com/channels/256692/fields/3/last.json";
+	light(loc);
+}
